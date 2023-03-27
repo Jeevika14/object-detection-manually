625d09467cdefe3c3a813f6e40b9302987a7e60e
Jeevika14 <91791370+Jeevika14@users.noreply.github.com>
2023-03-27 19:42:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 img="";
 
+Status= "";
+
 function preload()
 {
     img= loadImage('dog_cat.jpg');
@@ -9,6 +11,9 @@ function setup()
 {
     canvas= createCanvas(640, 420);
     canvas.center()
+
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "status: Detecting objects"; 
 }
 
 function draw()
@@ -19,4 +24,26 @@ function draw()
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("cat",286, 84);
+    noFill();
+    stroke("#FF0000");
+    rect(60, 90, 550, 450);
+}
+
+function modelLoaded()
+{
+    console.log('model Loaded');
+    Status= true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
